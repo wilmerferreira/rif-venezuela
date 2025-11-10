5a9e70ba4e3e4951840594bb01873daec55d458e
Wilmer Ferreira <11060801+wilmerferreira@users.noreply.github.com>
2025-11-10 21:06:22 +0000

    initial implementation of the rif type with validations and unit tests

--- RifVenezuela.Tests/RifTests.cs
+++ RifVenezuela.Tests/RifTests.cs
@@ -11,4 +11,46 @@ public class RifTests
 
         action.ShouldNotThrow();
     }
+
+    [Fact]
+    public void Constructor_WithInvalidKind_ShouldThrowArgumentOutOfRangeException()
+    {
+        Should.Throw<ArgumentOutOfRangeException>(() => new Rif('X', 12345678));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(100_000_000)]
+    public void Constructor_WithInvalidIdentifier_ShouldThrowArgumentOutOfRangeException(int identifier)
+    {
+        Should.Throw<ArgumentOutOfRangeException>(() => new Rif('J', identifier));
+    }
+
+    [Theory]
+    [InlineData('G', 20000004, "G-20000004-0")]
+    [InlineData('G', 20000003, "G-20000003-1")]
+    [InlineData('G', 20000008, "G-20000008-2")]
+    [InlineData('G', 20000002, "G-20000002-3")]
+    [InlineData('G', 20000007, "G-20000007-4")]
+    [InlineData('G', 20000001, "G-20000001-5")]
+    [InlineData('G', 20000006, "G-20000006-6")]
+    [InlineData('G', 20000014, "G-20000014-7")]
+    [InlineData('G', 20000005, "G-20000005-8")]
+    [InlineData('G', 20000013, "G-20000013-9")]
+    [InlineData('J', 00343994, "J-00343994-0")]
+    [InlineData('J', 30399491, "J-30399491-1")]
+    [InlineData('J', 30438135, "J-30438135-2")]
+    [InlineData('J', 30468971, "J-30468971-3")]
+    [InlineData('J', 00002950, "J-00002950-4")]
+    [InlineData('J', 07013380, "J-07013380-5")]
+    [InlineData('J', 09512460, "J-09512460-6")]
+    [InlineData('J', 00008933, "J-00008933-7")]
+    [InlineData('J', 30142060, "J-30142060-8")]
+    [InlineData('J', 00006372, "J-00006372-9")]
+    public void Constructor_WithValidTypeAndNumber_CreatesRif(char type, int identifier, string expectedRif) 
+    {
+        var rif = new Rif(type, identifier);
+        rif.ToString().ShouldBe(expectedRif);
+    }
 }
--- RifVenezuela/Rif.cs
+++ RifVenezuela/Rif.cs
@@ -1,6 +1,85 @@
-﻿namespace RifVenezuela
+﻿using System;
+using System.Linq;
+
+namespace RifVenezuela
 {
+    /// <summary>
+    /// Represents a Registro de Información Fiscal (RIF).
+    /// </summary>
     public struct Rif
     {
+        private char _kind;
+        private int _identifier;
+        private byte _checksum;
+
+        /// <summary>
+        /// Initializes a new instance of the RifVenezuela.Rif structure by using a kind and identifier.
+        /// </summary>
+        /// <param name="kind">Subject's kind</param>
+        /// <param name="identifier">Subject's identifier</param>
+        /// <exception cref="ArgumentOutOfRangeException">When the identifier is not between 1 and 99,999,999</exception>
+        public Rif(char kind, int identifier)
+        {
+            if (identifier < 1 || identifier > 99_999_999)
+                throw new ArgumentOutOfRangeException(nameof(identifier), "Identifier must be between 1 and 99,999,999");
+
+            if (kind == 'v') kind = 'V';
+            if (kind == 'e') kind = 'E';
+            if (kind == 'j') kind = 'J';
+            if (kind == 'c') kind = 'C';
+            if (kind == 'p') kind = 'P';
+            if (kind == 'g') kind = 'G';
+
+            _kind = kind;
+            _identifier = identifier;
+            _checksum = Checksum(kind, identifier);
+        }
+
+        private static byte Checksum(char kind, int identifier)
+        {
+            var sum = 0;
+
+            if (kind == 'V') sum = 4;
+            else if (kind == 'E') sum = 8;
+            else if (kind == 'J' || kind == 'C') sum = 12;
+            else if (kind == 'P') sum = 16;
+            else if (kind == 'G') sum = 20;
+            else throw new ArgumentOutOfRangeException(nameof(kind), "Invalid rif type");
+
+            var digits = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            var position = 1;
+
+            while (identifier > 0)
+            {
+                digits[8 - position] = identifier % 10;
+                identifier /= 10;
+                position++;
+            }
+
+            sum += digits[0] * 3;
+            sum += digits[1] * 2;
+            sum += digits[2] * 7;
+            sum += digits[3] * 6;
+            sum += digits[4] * 5;
+            sum += digits[5] * 4;
+            sum += digits[6] * 3;
+            sum += digits[7] * 2;
+
+            var checksum = 11  - sum % 11;
+
+            if (checksum > 9)
+                checksum = 0;
+
+            return (byte)checksum;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the value of this instance in the standard format.
+        /// </summary>
+        /// <returns>The value of this RifVenezuela.Rif, formatted as follows: $-########-#</returns>
+        override public string ToString()
+        {
+            return $"{_kind}-{_identifier:D8}-{_checksum}";
+        }
     }
 }
